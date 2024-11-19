76092bbc0b9053593d3c10dce1c994af17222f07
RyanB-Rezende <ryan.b.rezende@gmail.com>
2024-11-19 20:40:13 -0300

    1

---
+++ app_dogs/lib/data/repositories/user_repository.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:path/path.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+class UserRepository {
+  Future<Database> initDb() async {
+    return openDatabase(
+      join(await getDatabasesPath(), 'doggie_database.db'),
+      version: 2,
+    );
+  }
+
+  Future<bool> emailExists(String email) async {
+    final db = await initDb();
+    final result =
+        await db.query('pessoas', where: 'email = ?', whereArgs: [email]);
+    return result.isNotEmpty;
+  }
+
+  Future<bool> userExistsByIdPessoa(int idPessoa) async {
+    final db = await initDb();
+    final result =
+        await db.query('login', where: 'id_pessoa =?', whereArgs: [idPessoa]);
+    return result.isNotEmpty;
+  }
+
+  insertUser(User user) async {
+    final db = await initDb();
+    final encryptedPassword =
+        sha256.convert(utf8.encode(user.senha)).toString();
+    await db.insert('login', user.toMap()..['senha'] = encryptedPassword);
+  }
+
+  Future<bool> verifyLogin(String usuario, String senha) async {
+    final db = await initDb();
+    final encryptedPassword = sha256.convert(utf8.encode(senha)).toString();
+    final result = await db.query('login',
+        where: 'usuario = ? AND senha = ?',
+        whereArgs: [usuario, encryptedPassword]);
+    return result.isNotEmpty;
+  }
+}
---
+++ app_dogs/lib/presentation/viewmodels/user_viewmodel.dart
@@ -0,0 +1,21 @@
+import 'package:app_dogs/data/repositories/user_repository.dart';
+
+class UserViewmodel {
+  final UserRepository repository;
+
+  UserViewmodel(this.repository);
+
+  Future<String> registerUser(
+      String email, String usuario, String senha) async {
+    final emailExists = await repository.emailExists(email);
+
+    if (emailExists) {
+      final db = await repository.initDb();
+      final result =
+          await db.query('pessoas', where: 'email = ?', whereArgs: [email]);
+      final idPessoa = result[0]['id'] as int;
+
+      final userAlreadyExists
+    }
+  }
+}
